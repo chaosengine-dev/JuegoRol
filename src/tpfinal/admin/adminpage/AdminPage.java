47cc25bcb617aa55c563c8011b1e24ca548f41ac
German Bermudez <128097692+German183@users.noreply.github.com>
2023-06-08 21:36:10 -0300

    seteo la fuente del titulo de AdminPage desde la fuente guardada en recursos

--- src/tpfinal/admin/adminpage/AdminPage.java
+++ src/tpfinal/admin/adminpage/AdminPage.java
@@ -2,6 +2,8 @@ package tpfinal.admin.adminpage;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.uiDesigner.core.Spacer;
+import tpfinal.graficos.CanvasVentana;
+import tpfinal.persistencia.LeerArchivos;
 import tpfinal.vistas.AdministrarVentanas;
 import tpfinal.vistas.VentanaJuego;
 
@@ -19,6 +21,7 @@ public class AdminPage extends JFrame implements VentanaJuego {
     private JButton jugarButton;
     private JButton gestionarUsuariosButton;
     private JLabel titulo;
+    public static Font fuente = LeerArchivos.leerFuente("Recursos/Fuentes/Enchanted Land.otf");
 
     public AdminPage() {
         add(ventana);
@@ -26,6 +29,7 @@ public class AdminPage extends JFrame implements VentanaJuego {
         setLocationRelativeTo(null); // centra la ventana en la pantalla
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // cierra el programa al cerrar la ventana
         setVisible(true); // hace visible la ventana
+        titulo.setFont(fuente.deriveFont(72f)); // setear la fuente y el tamaño, porque sino en las compus que no tengan agregada la fuente a windows, no se les ve con el formato que queremos
 
         setTitle("Admin Page"); // titulo de la ventana
 
@@ -97,6 +101,7 @@ public class AdminPage extends JFrame implements VentanaJuego {
         gestionarUsuariosButton.setBackground(new Color(-16777216));
         gestionarUsuariosButton.setBorderPainted(false);
         gestionarUsuariosButton.setContentAreaFilled(false);
+        gestionarUsuariosButton.setFocusPainted(false);
         Font gestionarUsuariosButtonFont = this.$$$getFont$$$("Enchanted Land", -1, 24, gestionarUsuariosButton.getFont());
         if (gestionarUsuariosButtonFont != null) gestionarUsuariosButton.setFont(gestionarUsuariosButtonFont);
         gestionarUsuariosButton.setForeground(new Color(-1));
